a4dfe931b91e9a066d7ea664a86cd95f28492a02
Shritesh99 <Shritesh.sj@gmail.com>
2018-07-01 21:11:50 +0530

    Initial commit

---
+++ space.rb
@@ -0,0 +1,19 @@
+require 'gosu'
+
+class Tutorial < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "My Space Game"
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+
+end
+
+Tutorial.new.show
